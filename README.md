3af406901c334a59290fa723eb27e8ccd0b4b784
Slawek Walkowski <swalk@google.com>
2019-04-11 13:28:25 +0200

    Adds Functions Framework API for Java 8.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Functions Framework API for Java 8
+Definitions of types used in function signatures in Functions Framework for Java 8.
+
+* `com.google.cloud.functions.Context` - optional second argument of a background function.
---
+++ functions-framework-api/src/main/java/com/google/cloud/functions/Context.java
@@ -0,0 +1,30 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.functions;
+
+/** An interface for event function context. */
+public interface Context {
+  /** Returns event ID. */
+  String eventId();
+
+  /** Returns event timestamp. */
+  String timestamp();
+
+  /** Returns event type. */
+  String eventType();
+
+  /** Returns event resource. */
+  String resource();
+}
