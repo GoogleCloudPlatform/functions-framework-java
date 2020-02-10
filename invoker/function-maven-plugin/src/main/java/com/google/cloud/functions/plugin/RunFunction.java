97025cb7ec6bd1271369d66a31c8cc2791e9ee1e
emcmanus <emcmanus@google.com>
2020-02-10 13:39:28 -0800

    Initial version of a Maven plugin to run functions.
    When appropriately configured in pom.xml, you can use `mvn function:run` to
    compile your function and start a web server that serves it.
    
    The version of the plugin artifact will track the version of the Invoker (aka Functions Framework). Since the plugin depends on and runs the Invoker, any time we change the latter we need to change the former.
    
    Further documentation to follow.
    
    PiperOrigin-RevId: 294293170

---
+++ invoker/function-maven-plugin/src/main/java/com/google/cloud/functions/plugin/RunFunction.java
@@ -0,0 +1,58 @@
+package com.google.cloud.functions.plugin;
+
+import com.google.cloud.functions.invoker.runner.Invoker;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(name = "run",
+    defaultPhase = LifecyclePhase.GENERATE_RESOURCES,
+    requiresDependencyResolution = ResolutionScope.COMPILE,
+    requiresDependencyCollection = ResolutionScope.COMPILE)
+@Execute(phase = LifecyclePhase.COMPILE)
+public class RunFunction extends AbstractMojo {
+
+  /**
+   * The name of the function to run. This is the name of a class that implements one of the
+   * interfaces in {@code com.google.cloud.functions}.
+   */
+  @Parameter(property = "run.functionTarget")
+  private String functionTarget;
+
+  /**
+   * The port on which the HTTP server wrapping the function should listen.
+   */
+  @Parameter(property = "run.port", defaultValue = "8080")
+  private Integer port;
+
+  /**
+   * Used to determine what classpath needs to be used to load the function. This parameter is
+   * injected by Maven and can't be set explicitly in a pom.xml file.
+   */
+  @Parameter(defaultValue = "${project.compileClasspathElements}", readonly = true, required = true)
+  private List<String> compilePath;
+
+  public void execute() throws MojoExecutionException {
+    String classpath = String.join(File.pathSeparator, compilePath);
+    List<String> args = new ArrayList<>();
+    args.addAll(Arrays.asList("--classpath", classpath));
+    if (functionTarget != null) {
+      args.addAll(Arrays.asList("--target", functionTarget));
+    }
+    try {
+      getLog().info("Calling Invoker with " + args);
+      Invoker.main(args.toArray(new String[0]));
+    } catch (Exception e) {
+      getLog().error("Could not invoke function: " + e, e);
+      throw new MojoExecutionException("Could not invoke function", e);
+    }
+  }
+}
