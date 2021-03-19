e53e3b242dff2f9f49e79806411bee7e079ebf41
Matthew Robertson <mattrobertson@google.com>
2021-03-19 13:53:33 -0700

    Add conformance tests (#84)
    * Add conformance tests
    
    * add github action
    
    * Address code review comments
    
    Typos, target version, try with resources, java time
    
    * Use GoogleCloudPlatform/functions-framework-conformance/action@v0.3.8

---
+++ invoker/conformance/src/main/java/com/google/cloud/functions/conformance/CloudEventsConformanceFunction.java
@@ -0,0 +1,77 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.functions.conformance;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import com.google.cloud.functions.CloudEventsFunction;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import io.cloudevents.CloudEvent;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * This class is used by the Functions Framework Conformance Tools to validate
+ * the framework's Cloud Events API. It can be run with the following command:
+ *
+ * <pre>{@code
+ * $ functions-framework-conformance-client \
+ *   -cmd="mvn function:run -Drun.functionTarget=com.google.cloud.functions.conformance.CloudEventsConformanceFunction" \
+ *   -type=cloudevent \
+ *   -buildpacks=false \
+ *   -validate-mapping=false \
+ *   -start-delay=5
+ * }</pre>
+ */
+public class CloudEventsConformanceFunction implements CloudEventsFunction {
+
+  private static final Gson gson = new GsonBuilder()
+      .serializeNulls()
+      .setPrettyPrinting()
+      .create();
+
+  @Override
+  public void accept(CloudEvent event) throws Exception {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter("function_output.json"))) {
+      writer.write(serialize(event));
+    }
+  }
+
+  /**
+   * Create a structured JSON representation of a cloud event
+   */
+  private String serialize(CloudEvent event) {
+    JsonObject jsonEvent = new JsonObject();
+
+    jsonEvent.addProperty("id", event.getId());
+    jsonEvent.addProperty("source", event.getSource().toString());
+    jsonEvent.addProperty("type", event.getType());
+    jsonEvent.addProperty("datacontenttype", event.getDataContentType());
+    jsonEvent.addProperty("subject", event.getSubject());
+    jsonEvent.addProperty("specversion", event.getSpecVersion().toString());
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+    jsonEvent.addProperty("time", event.getTime().format(formatter));
+
+    String payloadJson = new String(event.getData().toBytes(), UTF_8);
+    JsonObject jsonObject = new Gson().fromJson(payloadJson, JsonObject.class);
+    jsonEvent.add("data", jsonObject);
+
+    return gson.toJson(jsonEvent);
+  }
+}
---
+++ invoker/conformance/src/main/java/com/google/cloud/functions/conformance/HttpConformanceFunction.java
@@ -0,0 +1,51 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.functions.conformance;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * This class is used by the Functions Framework Conformance Tools to validate
+ * the framework's HTTP API. It can be run with the following command:
+ *
+ * <pre>{@code
+ * $ functions-framework-conformance-client \
+ *   -cmd="mvn function:run -Drun.functionTarget=com.google.cloud.functions.conformance.HttpConformanceFunction" \
+ *   -type=http \
+ *   -buildpacks=false \
+ *   -validate-mapping=false \
+ *   -start-delay=5
+ * }</pre>
+ */
+public class HttpConformanceFunction implements HttpFunction {
+
+  @Override
+  public void service(HttpRequest request, HttpResponse response)
+      throws IOException {
+    try (BufferedReader reader = request.getReader();
+        BufferedWriter writer = new BufferedWriter(new FileWriter("function_output.json"))) {
+      int c;
+      while ((c = reader.read()) != -1) {
+        writer.write(c);
+      }
+    }
+  }
+}
