cf0038ffbfa7196f27462e7fa528df3018e90f31
Matthew Robertson <mattrobertson@google.com>
2021-06-28 07:57:54 +0000

    unblock local development with the pubsub emulator
    Currently, the functions frameworks are dependent on some private
    dataplane event marshalling logic in order to correctly pass PubSub
    events to background functions. This commit implements the same
    marshalling logic in the FF in order to enable local development using
    the PubSub emulator.
    
    We have already made this change in other languages:
    https://github.com/GoogleCloudPlatform/functions-framework-nodejs/pull/272
    https://github.com/GoogleCloudPlatform/functions-framework-ruby/pull/100
    https://github.com/GoogleCloudPlatform/functions-framework-python/pull/121
    https://github.com/GoogleCloudPlatform/functions-framework-go/pull/70

--- invoker/core/src/test/java/com/google/cloud/functions/invoker/BackgroundFunctionExecutorTest.java
+++ invoker/core/src/test/java/com/google/cloud/functions/invoker/BackgroundFunctionExecutorTest.java
@@ -2,13 +2,21 @@ package com.google.cloud.functions.invoker;
 
 import static com.google.cloud.functions.invoker.BackgroundFunctionExecutor.backgroundFunctionTypeArgument;
 import static com.google.common.truth.Truth8.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import com.google.cloud.functions.BackgroundFunction;
 import com.google.cloud.functions.Context;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
 
 @RunWith(JUnit4.class)
 public class BackgroundFunctionExecutorTest {
@@ -61,5 +69,39 @@ public class BackgroundFunctionExecutorTest {
     Class<? extends BackgroundFunction<?>> c =
         (Class<? extends BackgroundFunction<?>>) (Class<?>) ForgotTypeParameter.class;
     assertThat(backgroundFunctionTypeArgument(c)).isEmpty();
+  }
+
+  @Test
+  public void parseLegacyEventPubSub() throws IOException {
+    try (Reader reader = new InputStreamReader(getClass().getResourceAsStream("/pubsub_background.json"))) {
+      Event event = BackgroundFunctionExecutor.parseLegacyEvent(reader);
+
+      Context context = event.getContext();
+      assertEquals("google.pubsub.topic.publish", context.eventType());
+      assertEquals("1", context.eventId());
+      assertEquals("2021-06-28T05:46:32.390Z", context.timestamp());
+
+      JsonObject data = event.getData().getAsJsonObject();
+      assertEquals(data.get("data").getAsString(), "eyJmb28iOiJiYXIifQ==");
+      String attr = data.get("attributes").getAsJsonObject().get("test").getAsString();
+      assertEquals(attr, "123");
+    }
+  }
+
+  @Test
+  public void parseLegacyEventPubSubEmulator() throws IOException {
+    try (Reader reader = new InputStreamReader(getClass().getResourceAsStream("/pubsub_emulator.json"))) {
+      Event event = BackgroundFunctionExecutor.parseLegacyEvent(reader);
+
+      Context context = event.getContext();
+      assertEquals("google.pubsub.topic.publish", context.eventType());
+      assertEquals("1", context.eventId());
+      assertNotNull(context.timestamp());
+
+      JsonObject data = event.getData().getAsJsonObject();
+      assertEquals(data.get("data").getAsString(), "eyJmb28iOiJiYXIifQ==");
+      String attr = data.get("attributes").getAsJsonObject().get("test").getAsString();
+      assertEquals(attr, "123");
+    }
   }
 }
