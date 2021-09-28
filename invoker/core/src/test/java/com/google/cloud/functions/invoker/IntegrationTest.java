34f22385a06f59439db259cdb05d21ede5e7bec6
Grant Timmerman <timmerman+devrel@google.com>
2021-09-28 16:18:56 -0700

    ci: minor linting
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

--- invoker/core/src/test/java/com/google/cloud/functions/invoker/IntegrationTest.java
+++ invoker/core/src/test/java/com/google/cloud/functions/invoker/IntegrationTest.java
@@ -355,11 +355,13 @@ public class IntegrationTest {
 
   private static int getJavaVersion() {
     String version = System.getProperty("java.version");
-    if(version.startsWith("1.")) {
+    if (version.startsWith("1.")) {
       version = version.substring(2, 3);
     } else {
       int dot = version.indexOf(".");
-      if(dot != -1) { version = version.substring(0, dot); }
+      if (dot != -1) {
+        version = version.substring(0, dot);
+      }
     } return Integer.parseInt(version);
   }
 
