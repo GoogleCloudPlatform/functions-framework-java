216d62b0e11df75aec793ecac3f0837b2eb72793
Pratiksha Kap <pratikshakap@google.com>
2022-07-06 20:00:47 +0000

    adding buildpack integration for java

---
+++ invoker/conformance/prerun.sh
@@ -0,0 +1,15 @@
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+rm -rf $REPO_ROOT/tmp
+mkdir $REPO_ROOT/tmp
+
+cp -r $REPO_ROOT/invoker/conformance $REPO_ROOT/tmp
+
+cd $REPO_ROOT/invoker
+mvn install -Dmaven.repo.local=$REPO_ROOT/tmp/conformance/artifacts
+
+cd $REPO_ROOT/functions-framework-api 
+mvn install -Dmaven.repo.local=$REPO_ROOT/tmp/conformance/artifacts
+
+rm $REPO_ROOT/tmp/conformance/pom.xml
+mv $REPO_ROOT/tmp/conformance/buildpack.xml $REPO_ROOT/tmp/conformance/pom.xml
