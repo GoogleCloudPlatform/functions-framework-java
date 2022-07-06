a6d59ebafd118802feaa0a83cce63b3083192cad
Pratiksha Kap <pratikshakap@google.com>
2022-07-06 22:55:28 +0000

    add comments

--- invoker/conformance/prerun.sh
+++ invoker/conformance/prerun.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+
+# This script is used for buildpack validation.
+# Its purpose is to run conformance tests using a buildpack
+# with the latest code of functions-framework.
+#
+# Note: buildpack_pom.xml contains the configuration to use the local versions
+# java-function-invoker and functions-framework-api. We will be using this file
+# for running our conformance tests with buildpack.
+#
+# Steps:
+#
+# - Clear out the temp directory
+# - Copy the conformance tests folder into temp
+# - Build java-function-invoker with version 1.1.1-SNAPSHOT into artifacts
+#  folder
+# - Build functions-framework-api with version 1.0.5-SNAPSHOT into artifacts 
+# folder
+# - Ensure that we use the buildpack_pom.xml file by renaming it to pom.xml
+
+
+set -e
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 rm -rf $REPO_ROOT/tmp
@@ -12,4 +34,4 @@ cd $REPO_ROOT/functions-framework-api
 mvn install -Dmaven.repo.local=$REPO_ROOT/tmp/conformance/artifacts
 
 rm $REPO_ROOT/tmp/conformance/pom.xml
-mv $REPO_ROOT/tmp/conformance/buildpack.xml $REPO_ROOT/tmp/conformance/pom.xml
+mv $REPO_ROOT/tmp/conformance/buildpack_pom.xml $REPO_ROOT/tmp/conformance/pom.xml
