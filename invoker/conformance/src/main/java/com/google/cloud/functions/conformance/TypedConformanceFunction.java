1f063061616cbe34b84b0a2a240c69fe6a43f8d4
Gareth George <garethgeorge@google.com>
2023-06-23 21:05:02 +0000

    fix: typed function signature should be compatible with 'http' signature type

--- invoker/conformance/src/main/java/com/google/cloud/functions/conformance/TypedConformanceFunction.java
+++ invoker/conformance/src/main/java/com/google/cloud/functions/conformance/TypedConformanceFunction.java
@@ -1,19 +1,25 @@
 package com.google.cloud.functions.conformance;
 
 import com.google.cloud.functions.TypedFunction;
-import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
 
-public class TypedConformanceFunction implements TypedFunction<JsonObject, ConformanceResponse> {
+public class TypedConformanceFunction implements TypedFunction<ConformanceRequest, ConformanceResponse> {
   @Override
-  public ConformanceResponse apply(JsonObject req) throws Exception {
+  public ConformanceResponse apply(ConformanceRequest req) throws Exception {
     return new ConformanceResponse(req);
   }
 }
 
-class ConformanceResponse {
-  JsonObject payload = null;
+class ConformanceRequest {
+  @SerializedName("message")
+  public String message;
+}
 
-  ConformanceResponse(JsonObject payload) {
+class ConformanceResponse {
+  @SerializedName("payload")
+  public ConformanceRequest payload = null;
+
+  ConformanceResponse(ConformanceRequest payload) {
     this.payload = payload;
   }
 }
