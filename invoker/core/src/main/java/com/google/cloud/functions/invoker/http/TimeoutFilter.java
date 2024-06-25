b29b1528fbafae7f5fba265be3fb3d32a0802aa3
janell-chen <122311137+janell-chen@users.noreply.github.com>
2024-06-25 13:47:10 -0700

    fix: server times out when specified by CLOUD_RUN_TIMEOUT_SECONDS

---
+++ invoker/core/src/main/java/com/google/cloud/functions/invoker/http/TimeoutFilter.java
@@ -0,0 +1,73 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.cloud.functions.invoker.http;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class TimeoutFilter implements Filter {
+  private static final int DEFAULT_TIMEOUT_SECONDS = 30; // Default timeout in seconds
+  private final int timeoutMs;
+
+  public TimeoutFilter() {
+    this(DEFAULT_TIMEOUT_SECONDS);
+  }
+
+  public TimeoutFilter(int timeoutSeconds) {
+    this.timeoutMs = timeoutSeconds * 1000; // Convert seconds to milliseconds
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    Timer timer = new Timer(true);
+    TimerTask timeoutTask =
+        new TimerTask() {
+          @Override
+          public void run() {
+            if (response instanceof HttpServletResponse) {
+              try {
+                ((HttpServletResponse) response)
+                    .sendError(HttpServletResponse.SC_REQUEST_TIMEOUT, "Request timed out");
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
+            } else {
+              try {
+                response.getWriter().write("Request timed out");
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
+            }
+          }
+        };
+
+    timer.schedule(timeoutTask, timeoutMs);
+
+    try {
+      chain.doFilter(request, response);
+      timeoutTask.cancel();
+    } finally {
+      timer.purge();
+    }
+  }
+}
